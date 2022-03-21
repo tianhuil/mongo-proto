a16d25c04c3f699581e06f81247b3eba93df44a5
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-03-20 22:17:05 -0400

    Add array assert for dot

--- src/orm/dot.assert.ts
+++ src/orm/dot.assert.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb'
 import * as ta from 'type-assertions'
 import { DotPaths } from './dot'
 
@@ -8,9 +9,13 @@ interface Example {
     d: {
       e: boolean
     }
+    f: ObjectId[]
   }
 }
 
 ta.assert<
-  ta.Equal<'_id' | 'a' | 'b' | 'b.c' | 'b.d.e' | 'b.d', DotPaths<Example>>
+  ta.Equal<
+    '_id' | 'a' | 'b' | 'b.c' | 'b.d.e' | 'b.d' | 'b.f' | `b.f.${number}`,
+    DotPaths<Example>
+  >
 >()
