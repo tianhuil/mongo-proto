f4c5dbd2f3fca4aba3a795821b4b7d034e805290
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-03-22 23:05:27 -0400

    Proejction

---
+++ src/orm/projection.ts
@@ -0,0 +1,19 @@
+import { WithOperator } from './filter'
+import { FlattenFilterPaths, FlattenFilterType } from './flatten'
+
+export declare type ProjectionOperator<Field> = Field extends ReadonlyArray<
+  infer ArrayType
+>
+  ? {
+      $elemMatch: WithOperator<ArrayType>
+      $slice?: number | [number, number]
+    }
+  : never
+
+export declare type Projection<TSchema> =
+  | {
+      [Property in FlattenFilterPaths<TSchema>]?: Property extends '_id'
+        ? 1 | 0 | boolean
+        : ProjectionOperator<FlattenFilterType<TSchema, Property>> | 1 | true
+    }
+  | { [Property in FlattenFilterPaths<TSchema>]?: 0 | false }
