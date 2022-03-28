2c5e1ac04b2d1261684e97ef1135afbf6157a021
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-03-27 20:50:36 -0400

    check that promise is called

--- test/middleware.test.ts
+++ test/middleware.test.ts
@@ -6,7 +6,7 @@ type Example = { a: number }
 const mkListeningCollection = async () => {
   const col = await mkTsTestCollection<Example>()
 
-  const [before, after] = [jest.fn(), jest.fn()]
+  const [before, after, callback] = [jest.fn(), jest.fn(), jest.fn()]
   addMiddleware(col, {
     handler:
       ({ originalMethod }) =>
@@ -14,26 +14,30 @@ const mkListeningCollection = async () => {
         before(args)
         const result = originalMethod(...args)
         after(result)
+        if (result?.then) {
+          result?.then((val: any) => callback(val))
+        }
         return result
       },
   })
 
-  return { col, before, after }
+  return { col, before, after, callback }
 }
 
 test('test listening on insertOne', async () => {
-  const { col, before, after } = await mkListeningCollection()
+  const { col, before, after, callback } = await mkListeningCollection()
   const { insertedId } = await col.insertOne({ a: 2 })
 
   expect(before).toHaveBeenCalledTimes(1)
   expect(before).toHaveBeenCalledWith([{ a: 2, _id: insertedId }])
   expect(after).toHaveBeenCalledTimes(1)
+  expect(callback).toHaveBeenCalledTimes(1)
 })
 
 test('test listening on find', async () => {
   const { col, before, after } = await mkListeningCollection()
 
-  for await (const x of col.find({})) {
+  for await (const _ of col.find({})) {
   }
   expect(before).toHaveBeenCalledTimes(1)
   expect(before).toHaveBeenCalledWith([{}])
